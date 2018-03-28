44a4d673b414fa1b06593ed551fedc93ec77cc4e
ViktorrSH <32921336+ViktorrSH@users.noreply.github.com>
2018-03-28 16:22:25 +0800

    второй комит дома одобрить

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from  article.models import Article, Comments
 # Register your models here.
+class ArticleInline(admin.StackedInline):
+    model = Comments
+    extra = 2
+class ArticleAdmin(admin.ModelAdmin):
+    fiels = ['article_title', 'article_text', 'article_date']
+    inlines = [ArticleInline]
+    filtr = ['article_date']
+
+admin.site.register(Article, ArticleAdmin)
